0251c58bfa9dc040bc051e6649540befa68c6fc1
nikunj <nikunjgoyal31@gmail.com>
2023-06-11 15:27:46 +0530

    feat: added tailwind for styling

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,11 +2,17 @@
   <div id="app">
     Let's create the four crud points first!
 
+    <span class="text-3xl font-bold underline">
+      yello
+    </span>
     <section>
       <h1>Create</h1>
+      <span class="text-3xl font-bold underline">Hi</span>
+      <button>Add new dish</button>
     </section>
     <section>
       <h1>Read</h1>
+      {{ dishes.map(dish => dish.name) }}
     </section>
     <section>
       <h1>Update</h1>
@@ -18,5 +24,9 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import { Dish } from './models/dishes';
+
+const dishes = ref<Array<Dish>>([])
 
 </script>
